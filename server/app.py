363dd0046c801f6bb10ae0f378f33e7d01205bb0
Henry <henry.bogita@student.moringaschool.com>
2024-07-06 13:07:06 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -3,20 +3,20 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
+# Initialize SQLAlchemy
 db.init_app(app)
+migrate = Migrate(app, db)
 
+# Create API object
 api = Api(app)
 
-
+# Resource for /plants endpoint
 class Plants(Resource):
 
     def get(self):
@@ -37,19 +37,43 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
-api.add_resource(Plants, '/plants')
-
-
+# Resource for /plants/<id> endpoint
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        return make_response(jsonify(plant.to_dict()), 200)
 
+    def put(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
 
+        plant.name = data.get('name', plant.name)
+        plant.image = data.get('image', plant.image)
+        plant.price = data.get('price', plant.price)
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get_or_404(id)
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
+
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+# Add resources to API
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
